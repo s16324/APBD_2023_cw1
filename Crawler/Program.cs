7c1cdd3e5e1b17640201525c55126927d88443f4
s16324 <s16324@pjwstk.edu.pl>
2022-02-06 20:11:50 +0100

    resolved

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -1,12 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Crawler
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            if (args.Length == 0)
+            {
+                throw new ArgumentException("Err: Brak argumentu (URL)");
+            }
+            String url = args[0];
+            Regex urlRegex = new Regex(@"^(http|ftp|https|www)://([\w+?\.\w+])+([a-zA-Z0-9\~\!\@\#\$\%\^\&\*\(\)_\-\=\+\\\/\?\.\:\;\'\,]*)?$", RegexOptions.IgnoreCase);//
+            Match urlMatch = urlRegex.Match(url);
+            if (!urlMatch.Success)
+            {
+                throw new ArgumentException("Err: Niepoprawny format URL.");
+            }
+
+            //Crawler
+
+            using (var hc = new HttpClient())
+            {
+
+                HttpResponseMessage response = await hc.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    string[] words = content.Split(' ');//split string with spaces
+
+                    Regex regex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);//define regex expr (ignore case)
+
+                    List<string> addressList = new List<string>();
+
+                    foreach (var word in words)
+                    {
+                        Match match = regex.Match(word);
+                        if (match.Success)
+                        {
+                            if (!addressList.Contains($"{match.Value}"))//dont add duplicates
+                            {
+                                addressList.Add($"{match.Value}");
+                            }
+                        }
+                    }
+
+                    if (addressList.Count == 0) { System.Console.WriteLine("Nie znaleziono adresów email"); }
+
+                    System.Console.WriteLine(String.Join("\n", addressList));//preint addresses
+
+                }
+                else
+                {
+                    throw new Exception("Błąd w czasie pobierania strony");
+                }
+            }
+
+            //hc.Dispose();
+            //1
+            //Console.WriteLine("Hello World!");
+
+
+
         }
     }
 }
